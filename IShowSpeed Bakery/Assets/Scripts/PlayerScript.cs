3057a7de34738b3d4d05fcf1c3ab122542f363d8
Moona-T <s2300927@edu.bc.fi>
2025-10-09 09:47:29 +0300

    Playerin pitäisi toimia täysin
    PlayerScripissä on pari lineä koodia joista voi poimia playerin pysöhtymisen mini peliä paletessa

--- IShowSpeed Bakery/Assets/Scripts/PlayerScript.cs
+++ IShowSpeed Bakery/Assets/Scripts/PlayerScript.cs
@@ -1,15 +1,22 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    //Minipeli movemet 
+    //FindObjectOfType<PlayerScript>().CanMove = false;  ||Kun minipeli alkaa
+    //FindObjectOfType<PlayerScript>().CanMove = true;   ||Kun minipeli loppuu
+
     public float moveSpeed = 10;
 
     float HorizontalInput;
     float VerticalInput;
 
+    [HideInInspector] public bool CanMove = true;
+
     Rigidbody rb;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -18,13 +25,20 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!CanMove)
+        {
+            // Pelaaja ei liiku, mutta gravity toimii
+            rb.linearVelocity = new Vector3(0, rb.linearVelocity.y, 0);
+            return;
+        }
+
         HorizontalInput = Input.GetAxis("Horizontal");
         VerticalInput = Input.GetAxis("Vertical");
 
-        // Liikesuunta suhteessa pelaajan forwardiin
+        // Liikkuminen
         Vector3 move = transform.right * HorizontalInput + transform.forward * VerticalInput;
 
-        // Asetetaan velocity (s�ilytet��n y-akselin arvo esim. gravitaatiolle)
+        // Asetetaan velocity 
         rb.linearVelocity = new Vector3(move.x * moveSpeed, rb.linearVelocity.y, move.z * moveSpeed);
     }
 }
